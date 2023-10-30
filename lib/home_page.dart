bfeee590f2448f6b7be5f87cdcb1cab889fd82a3
juiceewrld <darklordtriple6ix@mail.ru>
2023-10-30 17:59:01 +0300

    shkaf

---
+++ lib/home_page.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: Center(
+          child: Column(
+            textDirection: TextDirection.ltr,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            verticalDirection: VerticalDirection.down,
+            children: <Widget>[
+              Expanded(
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Container(
+                        color: Colors.yellow,
+                        margin: EdgeInsets.only(
+                            left: 8, top: 8, right: 4, bottom: 4),
+                      ),
+                    ),
+                    Expanded(
+                        child: Container(
+                      color: Colors.red,
+                      margin:
+                          EdgeInsets.only(left: 4, top: 8, right: 8, bottom: 4),
+                    )),
+                  ],
+                ),
+                flex: 3,
+              ),
+              Expanded(
+                  child: Row(
+                    children: [
+                      Expanded(
+                          child: Container(
+                        color: Colors.orange,
+                        margin: EdgeInsets.only(
+                            left: 8, top: 4, right: 4, bottom: 4),
+                      )),
+                      Expanded(
+                          child: Container(
+                        color: Colors.green,
+                        margin: EdgeInsets.only(
+                            left: 4, top: 4, right: 8, bottom: 4),
+                      )),
+                    ],
+                  ),
+                  flex: 1),
+              Expanded(
+                  child: Row(
+                    children: [
+                      Expanded(
+                          child: Container(
+                        color: Colors.black,
+                        margin: EdgeInsets.only(
+                            left: 8, top: 4, right: 4, bottom: 4),
+                      )),
+                      Expanded(
+                          child: Container(
+                        color: Colors.white,
+                        margin: EdgeInsets.only(
+                            left: 4, top: 4, right: 8, bottom: 4),
+                      )),
+                    ],
+                  ),
+                  flex: 1),
+              Expanded(
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Container(
+                      color: Colors.pink,
+                      margin:
+                          EdgeInsets.only(left: 8, top: 4, right: 4, bottom: 8),
+                    )),
+                    Expanded(
+                        child: Container(
+                      color: Colors.blue,
+                      margin:
+                          EdgeInsets.only(left: 4, top: 4, right: 8, bottom: 8),
+                    )),
+                  ],
+                ),
+                flex: 3,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
